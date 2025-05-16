1d75fe61ed980486059bd5283a91e5e2a8b992d2
sirjan01-cmd <sirachdeguzman7@gmail.com>
2025-05-15 22:38:35 -0500

    Practice Array

---
+++ javascript/ch1_alert.js
@@ -0,0 +1,340 @@
+// alert("Saying hi from different file");
+/*
+let a = 10; // set a value to 10
+console.log(a);
+// prompt ('Hi how are you?'); similar to alert, but takes input of the user
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+*/
+
+/*
+This is a string using backticks 
+
+let language = "Javascript";
+let message = `let's learn ${language}`;
+console.log(message);
+*/
+
+/*
+Type of string using backlash 
+
+let str = "Hello, what's your name? Is it \"Mike\"?";
+console.log(str);
+let str2 = 'Hello, what\'s your name? Is it "Mike"?';
+console.log(str2);
+*/
+
+/*
+type of string using \nline and \\
+
+let str3 = "New \nline.";
+let str4 = "I'm containing a backlash: \\!";
+console.log(str3);
+console.log(str4);
+*/
+
+/*
+sample of number data type
+
+let intNr = 1;
+let decNR = 1.5;
+let expNr = 1.4e15;
+let octNr = 0o10;
+let hexNr = 0x9E8;
+let biNr = 0b101;
+*/
+
+/*
+learning to used typeof 
+
+testVariable =1;
+variableTypeTest1 = typeof testVariable;
+variableTypeTest2 = typeof(testVariable);
+console.log(variableTypeTest1);
+console.log(variableTypeTest2);
+
+let str = "Hello";
+let nr = 7;
+let bigNr = 12345678901234n;
+let bool = true;
+let sym = Symbol("unique");
+let undef =  undefined;
+let unknown = null;
+
+console.log("str", typeof str);
+console.log("nr", typeof nr);
+console.log("bigNr", typeof bigNr);
+console.log("bool", typeof bool);
+console.log("sym", typeof sym);
+console.log("undef", typeof undef);
+console.log("unknown", typeof unknown);
+*/
+
+/*
+convertion of string to number, number to string variable to boolean
+
+let nrToStr = 6;
+nrToStr = String(nrToStr);
+console.log(nrToStr, typeof nrToStr);
+
+let strToNr = "12";
+strToNr = Number(strToNr);
+console.log(strToNr, typeof strToNr);
+
+let strToBool = "any string will return true"
+strToBool = Boolean(strToBool);
+console.log(strToBool, typeof strToBool);
+*/
+
+/*
+let nullToNr = null;
+nullToNr = Number(nullToNr);
+console.log("null", nullToNr, typeof nullToNr);
+
+let strToNr = "";
+strToNr = Number(strToNr);
+console.log("empty string", strToNr, typeof strToNr);
+
+let strToNr2 = "hello";
+strToNr2 = Number(strToNr2);
+console.log(strToNr2, typeof strToNr2);
+
+let strToBool2 = "false";
+strToBool2 = Boolean(strToBool2);
+console.log(strToBool2, typeof strToBool2);
+
+let strToBool = "";
+strToBool = Boolean(strToBool);
+console.log(strToBool, typeof strToBool);
+*/
+
+/*
+let nr1 = 2;
+let nr2 = "2"
+console.log(nr1 + Number(nr2));
+*/
+
+/*
+let str1 = 'Laurence';
+let str2 = "Svekis"
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+console.log(str1, typeof str1);
+console.log(str2, typeof str2);
+console.log(val1, typeof val1);
+console.log(val2, typeof val2);
+console.log(myNum, typeof myNum);
+*/
+
+/*
+let nr1 = 12;
+let nr2 = 14;
+let result1 = nr1 + nr2;
+console.log(result1)
+
+let str1 = "hello ";
+let str2 = "addition"
+let result2 = str1 + str2;
+console.log(result2);
+*/
+
+/*
+let name = "Sirach";
+let age = 32;
+let message = `Hello, My name is ${name}, I'm 32 years old and I can code Javascript: true`;
+console.log(message);
+console.log(name, age, message);
+*/
+
+/*
+let nr1 = 20;
+let nr2 = 4;
+let str1 = "Hi"
+let nr3 = 3;
+let result1 = nr1 - nr2;
+let result2 = str1 - nr3;
+console.log(result1, result2);
+
+let nr4 = 20;
+let nr5 = 4;
+let str2 = "Hi"
+let nr6 = 3;
+let result3 = nr4 * nr5;
+let result4 = str2 * nr6;
+console.log(result3, result4);
+
+let nr7 = 30;
+let nr8 = 5;
+let result5 = nr7/nr8;
+console.log(result5);
+*/
+
+/*
+let nr1 = 2;
+let nr2 = 3;
+let result1 = nr1 ** nr2;
+console.log(result1);
+*/
+/*
+let nr1 = 10;
+let nr2 = 3;
+let result1 = nr1 % nr2;
+console.log(`${nr1} % ${nr2} = ${result1}`);
+*/
+
+/*
+let nr1 = 4;
+nr1++;
+console.log(nr1);
+
+let nr2 = 4;
+nr2--;
+console.log(nr2);
+
+let nr = 2;
+console.log(nr++);
+console.log(nr);
+*/
+
+/*
+let nr = 2;
+console.log(++nr);
+*/
+
+/*
+let nr1 = 4;
+let nr2 = 5;
+let nr3 = 2;
+console.log(nr1++ + ++nr2 * nr3++);
+*/
+
+/*
+function calculateHypotenuse(a, b) {
+  const c = Math.sqrt(a ** 2 + b ** 2);
+  return c;
+}
+
+const sideaA = 5;
+const sideB = 12;
+
+const hypotenuse =  calculateHypotenuse(sideaA, sideB);
+console.log("The hypotenuse is:", hypotenuse);
+*/
+
+/*
+let a = 4;
+let b = 3;
+let c = 2;
+console.log(b += a);
+console.log(a /= c);
+*/
+
+/*
+let x = 5;
+let y = 6;
+console.log(y > x);
+console.log(x > y);
+console.log(y > y);
+console.log(y >= y);
+console.log(y < x);
+console.log(x < y);
+console.log(y < y);
+console.log(y <= y);
+*/
+
+/*
+let x = 1;
+let y = 2;
+let z = 3;
+console.log(x < y && y < z);
+
+console.log(x > y && y < z);
+
+console.log(x > y || y < z);
+*/
+
+/*
+let x =  false;
+console.log(!x);
+*/
+
+/*
+negating boolean
+
+let x = 1;
+let y = 2;
+console.log(!(x < y));
+*/
+
+/*
+creating BMI
+
+const inches = 65;
+const pounds =  180;
+
+const inchToCm = 2.54;
+const poundsToKilo = 2.2046;
+
+const heightInCm = inches * inchToCm;
+const heightInMeters = heightInCm / 100;
+
+const weightInKilos = pounds / poundsToKilo;
+
+const bmi = weightInKilos / (heightInMeters ** 2);
+
+console.log(`Height in meters: ${heightInMeters.toFixed(2)} m`);
+console.log(`Weight in kilos: ${weightInKilos.toFixed(2)} kg`);
+console.log(`BMI: ${bmi.toFixed(2)}`);
+*/
+
+/*
+Creating an array
+
+let arr = ["Hi there", 5, true];
+console.log(typeof arr[0]);
+console.log(typeof arr[1]);
+console.log(typeof arr[2]);
+
+Improper way of adding values in an array
+cars = ["Toyota", "Renault", "Volkswagen"];
+cars[0] = "Tesla"
+cars[3] = "Kia"
+cars[-1] = "Fiat"
+console.log(cars[0]);
+console.log(cars[3])
+console.log(cars[-1]);
+*/
+
+/*
+colors = ["black", "orange", "pink"];
+booleans = [true, false, false,true];
+emptyArray = [];
+
+console.log("Length of colors:", colors.length);
+console.log("Length of booleans:", booleans.length);
+console.log("Length of empty array:", emptyArray.length);
+
+numbers = [12, 24, 36];
+numbers[5] = 48;
+console.log(numbers.length);
+console.log("numbers", numbers);
+
+improper way of adding elements using new indices
+
+shoppingList = ["Milk", "Bread", "Apples"]
+console.log(shoppingList);
+shoppingList[1] = "Bananas"
+console.log(shoppingList[1]);
+console.log(shoppingList);
+*/
+
+favoriteFruits = ["grapefruit", "orange", "lemon"];
+favoriteFruits.push("tangerine");
+let lengthofFavoriteFruits = favoriteFruits.push("lime")
+console.log(favoriteFruits);
+
+let arrOfShapes = ["circle", "triangle", "rectangle", "pentagon"];
+arrOfShapes.splice(2, 0, "square", "trapezoid");
+console.log(arrOfShapes);
