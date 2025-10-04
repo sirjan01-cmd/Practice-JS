0007606e0bd0c71829ce898f19022c6d1dafd0cc
sirjan01-cmd <sirachdeguzman7@gmail.com>
2025-10-04 08:00:22 -0500

    remove comment

--- javascript/builtInJSMethod.js
+++ javascript/builtInJSMethod.js
@@ -26,24 +26,26 @@ console.log("Encoded string:", encodeURIComponent(normalStr));
 let webURI = "http://www.basescript.com?data=Hello World";
 
 console.log("Encoded web URI:", encodeURIComponent(webURI));
+*/
 
 let str_int = "6";
 let int_int = parseInt(str_int);
 console.log("Type of ", int_int, " is ", typeof int_int);
 
 let str_float = "7.6";
-let in_float = parseFloat(str_float);
-console.log("Type of ", in_float, " is ", typeof in_float);
+let int_float = parseInt(str_float);
+console.log("Type of ", int_float, " is ", typeof int_float);
 
 let str_binary = "0b101";
 let int_binary = parseInt(str_binary);
 console.log("Type of ", int_binary, " is ", typeof int_binary);
-*/
+
 
 let uri = "https://www.JanineVizcarra.com/submit?name=janine vizcarra";
 let encoded_uri = encodeURI(uri);
 console.log("Encoded:", encoded_uri);
 let decoded_uri = decodeURI(encoded_uri);
 console.log("Decoded:", decoded_uri);
+
 
 // Page 167
